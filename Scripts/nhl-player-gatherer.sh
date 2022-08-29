090a8a8c82847639b706c426d8492138b2950251
Cole Titze <cole.titze@outlook.com>
2022-08-28 20:47:50 -0500

    Fixed path

--- Scripts/nhl-player-gatherer.sh
+++ Scripts/nhl-player-gatherer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/nhl-player-gatherer && git pull
-cd ~/source/repos/nhl-player-gatherer && dotnet build LocalRunning --configuration Release --output ~/Releases/nhl-player-gatherer/
+cd ~/source/repos/nhl-player-gatherer/nhl-player-trigger && dotnet build LocalRunning --configuration Release --output ~/Releases/nhl-player-gatherer/
 ~/Releases/nhl-player-gatherer/LocalRunning
