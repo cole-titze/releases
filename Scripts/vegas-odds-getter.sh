3bd114a329ac75ed111a8e57c04cf8879ec62fb6
Cole Titze <cole.titze@outlook.com>
2022-09-03 22:12:56 -0500

    Added export

--- Scripts/vegas-odds-getter.sh
+++ Scripts/vegas-odds-getter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ~/source/repos/vegas-odds-getter && git pull
 cp -r ~/source/repos/vegas-odds-getter/* ~/releases/vegas-odds-getter/
+export PYTHONPATH="${PYTHONPATH}:~/releases/vegas-odds-getter"
 cd ~/releases/vegas-odds-getter && python3 LocalRunning/local_running.py
