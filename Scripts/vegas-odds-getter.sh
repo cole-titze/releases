68666885e4e784ee5a2cbf50052c20edf9ecf8cb
Cole Titze <cole.titze@outlook.com>
2022-09-03 19:11:35 -0500

    Updated releases folder name

--- Scripts/vegas-odds-getter.sh
+++ Scripts/vegas-odds-getter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/vegas-odds-getter && git pull
-cp -r ~/source/repos/vegas-odds-getter/* ~/Releases/vegas-odds-getter/
+cp -r ~/source/repos/vegas-odds-getter/* ~/releases/vegas-odds-getter/
 ~/releases/nhl-player-gatherer/LocalRunning
