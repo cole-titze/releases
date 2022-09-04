c307295d71b10fe887592e7c635e66cf2c404934
Cole Titze <cole.titze@outlook.com>
2022-09-03 21:32:25 -0500

    Updated paths

--- Scripts/vegas-odds-getter.sh
+++ Scripts/vegas-odds-getter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/vegas-odds-getter && git pull
 cp -r ~/source/repos/vegas-odds-getter/* ~/releases/vegas-odds-getter/
-~/releases/nhl-player-gatherer/LocalRunning
+cd ~/releases/vegas-odds-getter && python3 LocalRunning/local_running.py
