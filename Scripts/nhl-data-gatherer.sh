b645e000ae173d7d298a2ada4fcead36d4c123d9
Cole Titze <cole.titze@outlook.com>
2022-09-03 19:05:02 -0500

    Added vegas odds getter and changed repo names

--- Scripts/nhl-data-gatherer.sh
+++ Scripts/nhl-data-gatherer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/nhl-data-gatherer && git pull
 cd ~/source/repos/nhl-data-gatherer && dotnet build LocalRunning --configuration Release --output ~/Releases/nhl-data-gatherer/
-~/Releases/nhl-data-gatherer/LocalRunning
+~/releases/nhl-data-gatherer/LocalRunning
---
+++ Scripts/vegas-odds-getter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/source/repos/vegas-odds-getter && git pull
+cp -r ~/source/repos/vegas-odds-getter/* ~/Releases/vegas-odds-getter/
+~/releases/nhl-player-gatherer/LocalRunning
