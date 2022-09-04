fd1a022204e4c9df6cdba17a7af228c32a0f6caf
Cole Titze <cole.titze@outlook.com>
2022-09-03 21:36:51 -0500

    Updated Releases to releases

--- Scripts/nhl-data-gatherer.sh
+++ Scripts/nhl-data-gatherer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/nhl-data-gatherer && git pull
-cd ~/source/repos/nhl-data-gatherer && dotnet build LocalRunning --configuration Release --output ~/Releases/nhl-data-gatherer/
+cd ~/source/repos/nhl-data-gatherer && dotnet build LocalRunning --configuration Release --output ~/releases/nhl-data-gatherer/
 ~/releases/nhl-data-gatherer/LocalRunning
--- Scripts/nhl-player-gatherer.sh
+++ Scripts/nhl-player-gatherer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/nhl-player-gatherer && git pull
-cd ~/source/repos/nhl-player-gatherer/nhl-player-trigger && dotnet build LocalRunning --configuration Release --output ~/Releases/nhl-player-gatherer/
+cd ~/source/repos/nhl-player-gatherer/nhl-player-trigger && dotnet build LocalRunning --configuration Release --output ~/releases/nhl-player-gatherer/
 ~/releases/nhl-player-gatherer/LocalRunning
