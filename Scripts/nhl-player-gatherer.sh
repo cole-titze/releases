38f92987e09a6970d18f565c77e9fe5a2c09da28
Cole Titze <cole.titze@outlook.com>
2022-09-05 15:02:43 -0500

    Update path

--- Scripts/nhl-player-gatherer.sh
+++ Scripts/nhl-player-gatherer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/source/repos/nhl-player-gatherer && git pull
-~/.dotnet/dotnet build ~/source/repos/nhl-player-gatherer/LocalRunning -a arm64 --self-contained true --configuration Release --output ~/releases/nhl-player-gatherer/
+~/.dotnet/dotnet build ~/source/repos/nhl-player-gatherer/nhl-player-trigger/LocalRunning -a arm64 --self-contained true --configuration Release --output ~/releases/nhl-player-gatherer/
 ~/releases/nhl-player-gatherer/LocalRunning
