f651f2ea0233515f40b11d32973a373b58307a6a
Cole Titze <cole.titze@outlook.com>
2022-09-05 17:44:50 -0500

    Updated path

--- Scripts/nhl-game-predictor.sh
+++ Scripts/nhl-game-predictor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/source/repos/nhl-game-predictor && git pull
-cp -r ~/source/repos/nhl-player-gatherer/* ~/releases/nhl-game-predictor/
+cp -r ~/source/repos/nhl-game-predictor/* ~/releases/nhl-game-predictor/
 export PYTHONPATH="${PYTHONPATH}:~/releases/nhl-game-predictor"
 cd ~/releases/nhl-game-predictor && python3 LocalRunning/local_running.py
