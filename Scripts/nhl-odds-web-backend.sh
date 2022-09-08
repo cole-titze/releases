10a78bd003af84068b6b2169c903ba33cd8063f4
Cole Titze <cole.titze@outlook.com>
2022-09-07 20:53:46 -0500

    Updated docker command

--- Scripts/nhl-odds-web-backend.sh
+++ Scripts/nhl-odds-web-backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker stop nhl-backend
+sudo docker rm nhl-backend
 cd ~/source/repos/nhl-odds-web-backend && git pull
 sudo docker build -t coledev/nhl-backend .
 sudo docker run -p 8080:80 -d --name nhl-backend coledev/nhl-backend
