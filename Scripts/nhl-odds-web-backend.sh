5300158de2e4a58c911b409b8794aa687e51d314
Cole Titze <cole.titze@outlook.com>
2022-09-21 20:28:11 -0500

    Restart always added to web containers

--- Scripts/nhl-odds-web-backend.sh
+++ Scripts/nhl-odds-web-backend.sh
@@ -2,4 +2,4 @@
 sudo docker stop odds-backend
 sudo docker rm odds-backend
 sudo docker pull ghcr.io/cole-titze/nhl-odds-web-backend:latest
-sudo docker run --name odds-backend --env-file ~/secrets/.env-backend -p 8080:80 -d ghcr.io/cole-titze/nhl-odds-web-backend:latest
+sudo docker run --name odds-backend --env-file ~/secrets/.env-backend -p 8080:80 --restart=always -d ghcr.io/cole-titze/nhl-odds-web-backend:latest
