51dc58c2fbaac7206473cebe9da888bf77a132b5
Cole Titze <cole.titze@outlook.com>
2023-04-30 16:58:22 -0500

    Updating to use new repos and scripts

---
+++ Scripts/nhl-odds-web-frontend.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo docker stop odds-frontend
+sudo docker rm odds-frontend
+sudo docker pull ghcr.io/cole-titze/nhl-odds-web-frontend:latest
+sudo docker run --name odds-frontend -p 8081:8081 --restart=always -d ghcr.io/cole-titze/nhl-odds-frontend:latest
--- Scripts/nhl-predictor-frontend-react.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker stop odds-frontend
-sudo docker rm odds-frontend
-sudo docker pull ghcr.io/cole-titze/nhl-predictor-frontend-react:latest
-sudo docker run --name odds-frontend -p 8081:8081 --restart=always -d ghcr.io/cole-titze/nhl-predictor-frontend-react:latest
