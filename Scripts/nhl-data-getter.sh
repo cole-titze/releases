2b757c7a68e2312d3e00af16c4f2a477c337b2a4
cole-titze <95884122+cole-titze@users.noreply.github.com>
2023-04-30 16:50:01 -0500

    Update nhl-data-getter.sh

--- Scripts/nhl-data-getter.sh
+++ Scripts/nhl-data-getter.sh
@@ -2,4 +2,4 @@
 sudo docker stop data-getter
 sudo docker rm data-getter
 sudo docker pull ghcr.io/cole-titze/nhl-data-getter:latest
-sudo docker run --name data-getter --env-file ~/secrets/.env-data-getter -d ghcr.io/cole-titze/nhl-data-gatherer:latest
+sudo docker run --name data-getter --env-file ~/secrets/.env-data-getter -d ghcr.io/cole-titze/nhl-data-getter:latest
