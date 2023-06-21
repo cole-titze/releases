a4d71d08e581775e41b6811f1245ce994901bace
Cole Titze <cole.titze@outlook.com>
2023-06-20 20:27:28 -0500

    adding ce

--- Scripts/portainer.sh
+++ Scripts/portainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker stop portainer
 sudo docker rm portainer
-sudo docker pull portainer/portainer:latest
+sudo docker pull portainer/portainer-ce:latest
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
