1b2d3db6d9d0c8610b4a46f5a08ec50977ff992b
cole-titze <95884122+cole-titze@users.noreply.github.com>
2023-10-22 21:07:52 -0500

    Update and rename nhl-vegas-odds-getter.sh to nhl-bookmaker-odds-getter.sh

---
+++ Scripts/nhl/nhl-bookmaker-odds-getter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo docker stop odds-getter
+sudo docker rm odds-getter
+sudo docker pull ghcr.io/cole-titze/nhl-bookmaker-odds-getter:latest
+sudo docker run --name odds-getter --env-file ~/secrets/.env-vegas-getter -d ghcr.io/cole-titze/nhl-bookmaker-odds-getter:latest
