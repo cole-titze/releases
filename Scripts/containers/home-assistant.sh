e7ea97d6022fb8695908128ac5417e206d48e8d5
Cole Titze <cole.titze@outlook.com>
2024-03-24 16:59:26 -0500

    Add skyconnect discovering

--- Scripts/containers/home-assistant.sh
+++ Scripts/containers/home-assistant.sh
@@ -2,4 +2,4 @@
 sudo docker stop home-assistant
 sudo docker rm home-assistant
 sudo docker pull homeassistant/raspberrypi4-homeassistant:latest
-sudo docker run -d --name home-assistant --restart=always -v /var/homeassistant:/config --network host homeassistant/raspberrypi4-homeassistant
+sudo docker run -d --device /dev/ttyUSB0:/dev/ttyUSB0 --name home-assistant --restart=always -v /var/homeassistant:/config --network host homeassistant/raspberrypi4-homeassistant
