85f2380185853e2018e407eb9756920c80be74b2
Cole Titze <cole.titze@outlook.com>
2024-04-25 21:02:19 -0500

    Add sleep to help with containers

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,6 @@
 sudo docker image prune -a -f
 sudo docker system prune --volumes -f
 sudo docker stop -t 600 $(docker ps -a -q)
+sleep 30
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo reboot
