1e55cae70cedb261207a2d4b15020f1457aeb906
Cole Titze <cole.titze@outlook.com>
2024-04-25 20:52:07 -0500

    remove now from reboot command

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -2,4 +2,4 @@ sudo docker image prune -a -f
 sudo docker system prune --volumes -f
 sudo docker stop -t 600 $(docker ps -a -q)
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo reboot now
+sudo reboot
