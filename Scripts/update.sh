45c735114ed55886196c257bc4f6e6c3c75a0970
Cole Titze <cole.titze@outlook.com>
2024-04-25 20:33:04 -0500

    Add sudo

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,5 @@
 sudo docker image prune -a -f
 sudo docker system prune --volumes -f
-docker stop -t 600 $(docker ps -a -q)
+sudo docker stop -t 600 $(docker ps -a -q)
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo reboot now
