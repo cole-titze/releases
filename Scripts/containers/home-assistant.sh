bea5d40edba45cb4feb211ba4ae261f52000f9c1
Cole Titze <cole.titze@outlook.com>
2024-04-25 20:17:36 -0500

    Updating docker stop time to give 10 minutes to help prevent home assistant from dying

--- Scripts/containers/home-assistant.sh
+++ Scripts/containers/home-assistant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker stop home-assistant
+sudo docker stop -t 600 home-assistant
 sudo docker rm home-assistant
 sudo docker pull homeassistant/raspberrypi4-homeassistant:latest
 sudo docker run -d --name home-assistant --restart=always --device /dev/ttyUSB0:/dev/ttyUSB0 -v /var/homeassistant:/config --network host homeassistant/raspberrypi4-homeassistant
--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,5 @@
 sudo docker image prune -a -f
 sudo docker system prune --volumes -f
-docker stop $(docker ps -a -q)
+docker stop -t 600 $(docker ps -a -q)
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo reboot now
